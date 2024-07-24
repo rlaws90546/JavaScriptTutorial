137a8ae6549c2bb9e631de2a537edace2f5e5ed6
Win Laws <wlaws999@gmail.com>
2024-07-24 17:33:52 -0400

    JS Functions lab final commit

--- calculateArea/calculateArea.js
+++ calculateArea/calculateArea.js
@@ -8,3 +8,16 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function groceryTracker() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = (grocery1 + grocery2 + grocery3).toFixed(2);
+    document.getElementById('total').innerText = `The total amount is: ${total}`;
+}
